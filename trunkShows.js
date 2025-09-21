45287ecc02324590d6da11337f7132726f28035f
Seb <88204170+IPQow@users.noreply.github.com>
2025-09-21 04:54:41 +0100

    Create trunkShows.js

---
+++ trunkShows.js
@@ -0,0 +1,44 @@
+[
+  {
+    "slug": "newyork",
+    "locationName": "New York",
+    "confirmed": true,
+    "venueName": "Metropolitan Club",
+    "addressLines": ["1 E 60th St", "New York, NY 10022"], // the new sections are new lines of the address, so ["first line", "second line"],
+    "dateStart": "23/10/2025", // dd/mm/yyyy
+    "dateEnd": "24/10/2025", // dd/mm/yyyy
+    "timeWindowLabel": "10:00 AM - 6:00 PM",
+    "dateLabel": "OCTOBER 23-24, 2025", // MONTH DD-DD, YYYY
+    "calNamespace": "newyork", // place
+    "calLink": "nichollsandco/newyork", // nichollsandco/place
+    "sortOrder": 1
+  },
+  {
+    "slug": "hongkong",
+    "locationName": "Hong Kong",
+    "confirmed": false,
+    "venueName": null,
+    "addressLines": null,
+    "dateStart": null,
+    "dateEnd": null,
+    "timeWindowLabel": null,
+    "dateLabel": null,
+    "calNamespace": null,
+    "calLink": "nichollsandco/hongkong",
+    "sortOrder": 2
+  },
+  {
+    "slug": "tokyo",
+    "locationName": "Tokyo",
+    "confirmed": false,
+    "venueName": null,
+    "addressLines": null,
+    "dateStart": null,
+    "dateEnd": null,
+    "timeWindowLabel": null,
+    "dateLabel": null,
+    "calNamespace": null,
+    "calLink": "nichollsandco/tokyo",
+    "sortOrder": 2
+  }
+]
